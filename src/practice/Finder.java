8401662baa81b5a1544b93e98ce8bbc47528128a
League <league@WorthyMini.local>
2019-12-28 11:45:41 -0800

    12-28-19

---
+++ src/practice/Finder.java
@@ -0,0 +1,12 @@
+package practice;
+
+import java.util.ArrayList;
+
+public class Finder {
+
+	public Object lastIndexOf(ArrayList<String> arrayList, String string) {
+		//break down to chars? Check String API for possible methods 
+		return null;
+	}
+
+}
--- src/practice/FishingContest.java
+++ src/practice/FishingContest.java
@@ -1,31 +1,42 @@
 package practice;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FishingContest {
-	HashMap <String, double[]> allCatches;
-	
+	HashMap<String, double[]> allCatches;
+
 	public FishingContest(HashMap<String, double[]> allCatches) {
-		this.allCatches = allCatches; 
+		this.allCatches = allCatches;
 	}
 
 	public double findBiggestFish(String string) {
-		double[] temp = allCatches.get(string);
+		double[] allFishes = allCatches.get(string);
 		double biggestNum = 0;
-		for (int i = 0; i < temp.length; i++) {
-			if(temp[i]>biggestNum) {
-				biggestNum = temp[i];
+		for (int i = 0; i < allFishes.length; i++) {
+			if (allFishes[i] > biggestNum) {
+				biggestNum = allFishes[i];
 			}
 		}
 		return biggestNum;
 	}
 
 	public Object findWinner() {
-		for(String name: allCatches.keySet()) {
-			findBiggestFish(name);
+		double overallBiggest = 0;
+		String overallWinner = "";
+		for (String name : allCatches.keySet()) {
+			double bigFish = 0;
+			double[] personsFishes = allCatches.get(name);
+			for (int i = 0; i < personsFishes.length; i++) {
+				if (personsFishes[i] > bigFish) {
+					bigFish = personsFishes[i];
+				}
+			}
+			if (bigFish > overallBiggest) {
+				overallBiggest = bigFish;
+				overallWinner = name;
+			}
 		}
-		//compare fishes 
-		return null;
+		return overallWinner;
 	}
-
 }
