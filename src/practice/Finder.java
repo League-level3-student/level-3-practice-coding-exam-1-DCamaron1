8444e219e3c4a49af254530e619530681d84f850
EmperorlNeo <neomartindelcampo2001@gmail.com>
2020-01-04 12:07:12 -0800

    12-4-20

--- src/practice/Finder.java
+++ src/practice/Finder.java
@@ -7,7 +7,6 @@ public class Finder {
 	public int lastIndexOf(ArrayList<String> arrayList, String string) {
 		int lastIndex = -1;
 		for (int i = 0; i < arrayList.size(); i++) {
-
 			if (arrayList.get(i).toLowerCase().equals(string.toLowerCase())) {
 				lastIndex = i;
 			}
