6618d18c7f8a6e55f3673d101a54a55a426bcc02
EmperorlNeo <neomartindelcampo2001@gmail.com>
2020-01-04 11:58:12 -0800

    12-4-20

--- src/practice/Finder.java
+++ src/practice/Finder.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 
 public class Finder {
 
-	public Object lastIndexOf(ArrayList<String> arrayList, String string) {
-		//break down to chars? Check String API for possible methods 
-		return null;
+	public int lastIndexOf(ArrayList<String> arrayList, String string) {
+		int lastIndex = -1;
+		for (int i = 0; i < arrayList.size(); i++) {
+
+			if (arrayList.get(i).toLowerCase().equals(string.toLowerCase())) {
+				lastIndex = i;
+			}
+		}
+		return lastIndex;
 	}
 
 }
